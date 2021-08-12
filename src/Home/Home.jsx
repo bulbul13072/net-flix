ee0db77f5e26de06f51325c417a756bcd165152d
bulbul13072 <mr.bulbul.rbl@gmail.com>
2021-08-13 01:36:35 +0600

    Second commit

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import Navbar from "../components/navbar/Navbar"
+import "./Home.scss"
 
 const Home = () => {
     return (
-        <div>
-            Hi Mama
+        <div className="Home">
+            <Navbar/>
         </div>
     )
 }

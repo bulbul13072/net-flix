927a730d419e0c6f7bdf7758e55b1ccac874334f
unknown <bulbul055@gmail.com>
2021-08-14 23:56:05 -0800

    advancement

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -4,6 +4,27 @@ import {InfoOutlined, PlayArrow} from "@material-ui/icons";
 export default function Featured({type}) {
     return (
         <div className="featured">
+            {type && (
+                <div className="category">
+                    <span> {type === "movie" ? "Movies" : "Series"} </span>
+                    <select name="genre" id="genre">
+                        <option> Genre </option>
+                        <option value="adventure">Adventure</option>
+                        <option value="comedy">Comedy</option>
+                        <option value="crime">Crime</option>
+                        <option value="fantacy">Fantacy</option>
+                        <option value="historical">Historical</option>
+                        <option value="horror">Horror</option>
+                        <option value="romance">Romance</option>
+                        <option value="sci-fi">Sci-fi</option>
+                        <option value="thriller">Thriller</option>
+                        <option value="western">Wesetern</option>
+                        <option value="animation">Animation</option>
+                        <option value="drama">Drama</option>
+                        <option value="documentary">Documentary</option>
+                    </select>
+                </div>
+            )}
             <img src="https://wegotthiscovered.com/wp-content/uploads/2019/03/Avengers-Engame-poster-cropped.jpg" alt="" />
             <div className="info">
                 <img src="https://www.district-geek.fr/wp-content/uploads/2019/09/avengers-endgame-title-logo.jpg" alt="" />
---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,9 @@
+import "./listItem.scss"
+
+export const ListItem = () => {
+    return (
+        <div className="listItem">
+            Item
+        </div>
+    )
+}
